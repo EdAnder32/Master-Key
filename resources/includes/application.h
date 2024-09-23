5f6def8d26e7158bb7a029793646798bfbe2d93e
Edmilson Alexandre <edalexan@c1r10s6.42luanda.com>
2024-09-23 12:18:03 +0100

    Removed old stuffs

--- resources/includes/application.h
+++
@@ -1,9 +0,0 @@
-#include "../lib/libsf.h"
-#include "../lib/libft.h"
-# include <stdlib.h>
-# include <fcntl.h>
-# include <unistd.h>
-
-void ft_render_objects(Window  *window);
-
-void on_button_click(Tool *widget, Pointer user_data);
